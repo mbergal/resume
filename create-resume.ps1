86f7bdf41b3d59e982bb71b0c2a47f5b53e58fa8
Misha Bergal <misha.bergal@gmail.com>
2012-03-15 03:04:28 -0500

    Generate HTML, Word and PDF documents

--- create-resume.ps1
+++ create-resume.ps1
@@ -1,23 +1,45 @@
-param( [Parameter(Mandatory=$true)][string]$source )
+#param( [Parameter(Mandatory=$true)][string]$source )
 
-$sourceDir = Split-Path -Parent $source
+$rootDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$source = Join-Path $rootDir "src\resume.js.net.xml"
 
-function ConvertSourceToHtml( $source )
+function ConvertSourceToHtml( $source, $destination )
     {
     $xslt = New-Object System.Xml.Xsl.XslCompiledTransform;
-    $xslt.Load("D:\Users\Misha\Dropbox\resume\resume-to-html.xsl" );
-    $xslt.Transform($source, "D:\Users\Misha\Dropbox\resume\resume.js.net.html" );    
+    $xslt.Load( ( Join-Path $rootDir "src\transformations\resume-to-html.xsl" ) );
+    $xslt.Transform( $source, $destination );    
     }
     
-function ConvertSourceToWord( $source )
+function ConvertSourceToWord( $source, $destination )
     {
+    $private:xslt = New-Object System.Xml.Xsl.XslCompiledTransform;
+    $xslt.Load( ( Join-Path $rootDir "src\transformations\resume-to-word.xsl" ) );
+    $tempFile = [System.IO.Path]::GetTempFileName()
+    $xslt.Transform( $source, $tempFile );    
     
+    $private:wrd = new-object -com word.application 
+    $wrd.visible = $true 
+    $doc = $wrd.documents.open( $tempFile )
+    $doc.SaveAs( $destination, 16 )
+    $doc.close()
+    $wrd.quit()
     }
 
-function ConvertSourceToPDF( $source )
+function ConvertSourceToPDF( $source, $destination )
     {
+    $private:xslt = New-Object System.Xml.Xsl.XslCompiledTransform;
+    $xslt.Load( ( Join-Path $rootDir "src\transformations\resume-to-word.xsl" ) );
+    $tempFile = [System.IO.Path]::GetTempFileName()
+    $xslt.Transform( $source, $tempFile );    
+    
+    $private:wrd = new-object -com word.application 
+    $wrd.visible = $true 
+    $doc = $wrd.documents.open( $tempFile )
+    $doc.SaveAs( $destination, 17 )
+    $doc.close()
+    $wrd.quit()
     }
     
-
-    
-ConvertSourceToHtml( $source )    
+ConvertSourceToHtml $source ( Join-Path $rootDir "bin\Resume.html" )
+ConvertSourceToWord $source ( Join-Path $rootDir "bin\Resume.docx" )
+ConvertSourceToPDF $source ( Join-Path $rootDir "bin\Resume.pdf" )
